3fe612eafb7e5ca2cb38b9d46be8d34523f1bd83
EdgarGarcia <garciamartinedgar@gmail.com>
2016-10-07 15:05:18 -0500

    programa 1

---
+++ ecuacion.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Math.h>
+
+int main()
+{
+int a = 5;
+int b = 3;
+int c = 1;
+int resultado = 0;
+ resultado = a +pow(b,2) - c;
+ printf("resultado",resultado );
+return 0;
+}
